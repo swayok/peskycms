048020aa5996769459f39ac05ba5854c77815e89
Alexander Filippov <swayalex@gmail.com>
2019-05-13 18:35:58 +0300

    cms:install command printed texts updates

--- src/PeskyCMS/Console/Commands/CmsInstallCommand.php
+++ src/PeskyCMS/Console/Commands/CmsInstallCommand.php
@@ -3,7 +3,6 @@
 namespace PeskyCMS\Console\Commands;
 
 use PeskyCMF\Console\Commands\CmfCommand;
-use PeskyCMS\Providers\PeskyCmsServiceProvider;
 use PeskyCMS\Scaffolds\CmsMenusScaffoldConfig;
 use PeskyCMS\Scaffolds\CmsNewsScaffoldConfig;
 use PeskyCMS\Scaffolds\CmsPagesScaffoldConfig;
@@ -41,7 +40,7 @@ class CmsInstallCommand extends CmfCommand {
             File::save($appSettingsFilePath, $contents);
             $this->line($appSettingsFilePath . ' updated to extend PeskyCmsAppSettings class instead of PeskyCmfAppSettings class.');
         } else {
-            $this->warn('Failed to locate AppSettings class using path ' . $appSettingsFilePath . '.');
+            $this->warn('- Failed to locate AppSettings class using path ' . $appSettingsFilePath . '.');
             $this->warn('To enable some CMS functionality you will need to extend PeskyCMS\PeskyCmsAppSettings by your AppSettings class' . $appSettingsFilePath . '.');
         }
         $migrationsPath = database_path('migrations') . DIRECTORY_SEPARATOR;
@@ -59,9 +58,8 @@ class CmsInstallCommand extends CmfCommand {
     }
 
     protected function outro() {
-        $this->line('1. Add ' . PeskyCmsServiceProvider::class . ' to you app.providers config (if you do not use package discovery)');
-        $this->line('2. Run "php artisan migrate" to create tables in database');
-        $this->line('3. Add next classes to your \'resourses\' array in admin\'s section config file (usually config/admin.php)');
+        $this->line('1. Run "php artisan migrate" to create tables in database');
+        $this->line('2. Add next classes to your \'resourses\' array in admin\'s section config file (usually config/admin.php)');
         $this->line('       \\' . CmsPagesScaffoldConfig::class . '::class,');
         $this->line('       \\' . CmsMenusScaffoldConfig::class . '::class,');
         $this->line('       \\' . CmsNewsScaffoldConfig::class . '::class,');
